4161bb096851251e8c4114e46eafeea6115ea9e5
10121044100 <ttm3244@gmail.com>
2017-08-28 23:44:53 +0900

    Compile OK

--- netfilter_test.c
+++ netfilter_test.c
@@ -14,6 +14,41 @@
 #include <dumpcode.h>
 
 /*
+ *	checksites
+ *	data check from block_list
+ *	This function similar to strstr() function.
+ */
+uint32_t 
+checksites(
+	const uint8_t* 	data, 
+	const uint8_t**	block_list,
+	const uint32_t	length)
+{
+	uint8_t* 	base;
+	uint8_t*	sample;
+
+	if(*data && **block_list) {
+		for(uint32_t i=0; *(block_list+i); i++) {
+			for(uint32_t j=0; j<length; j++) { 
+				base = (uint8_t*) data;
+				sample = (uint8_t*) *(block_list+i);
+		
+				while (*sample == *base &&
+					sample++ && 
+					base++) 
+				{ 
+					if (!*sample) return TRUE;
+				} 
+
+				data++; 
+			}
+		}
+	}
+
+	return FALSE; 
+}
+
+/*
  *	confirmblocking
  *	specific sites confirm.
  *	if site is included specific sites, return true(1).
@@ -24,13 +59,14 @@
 
 uint32_t 
 confirmblocking(
-	uint8_t*	payload, 
-	uint32_t 	length) 
+	const uint8_t*	payload, 
+	const uint32_t 	length) 
 {
 	struct iphdr	*ipptr = (struct iphdr *) payload;
 	struct tcphdr	*tcpptr = (struct tcphdr *) (payload + IPHDRLEN(ipptr));
 	uint8_t		*data = NULL;
 	uint32_t	data_length = 0;
+	const uint8_t	*block_list[2] = {(uint8_t*)"avnana.com", };
 
 	// Check TCP from ipptr	
 	if(ipptr->protocol == IPPROTO_TCP) {
@@ -38,9 +74,9 @@ confirmblocking(
 			data = (uint8_t*) (tcpptr + TCPHDRLEN(tcpptr));
 			data_length = length - IPHDRLEN(ipptr) - TCPHDRLEN(tcpptr);
 
-			dumpcode(data, data_length);
-
-			return TRUE;
+			//dumpcode(data, data_length);
+			if(checksites(data, block_list, data_length))
+				return TRUE;
 		}
 	}
 
@@ -65,6 +101,7 @@ callback(
 	u_int32_t payload_length;
 	unsigned char *payload;
 	u_int32_t isblocksite = 0;	// True & false
+	nfmsg->nfgen_family = 0;
 
 	printf("entering callback\n");
 	
--- netfilter_test.h
+++ netfilter_test.h
@@ -6,7 +6,8 @@
 #define FALSE	0
 
 int32_t callback(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *nfa, void *data);
-uint32_t confirmblocking(unsigned char* payload, uint32_t length);
+uint32_t confirmblocking(const uint8_t* payload, const uint32_t length);
+uint32_t checksites(const uint8_t* data, const uint8_t** block_list, const uint32_t length);
 #endif
 
 
